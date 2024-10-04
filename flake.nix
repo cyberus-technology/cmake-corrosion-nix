863f827b6c64a52edfa86adc4073be2961383fc1
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-10-04 15:58:37 +0200

    flake: add corrosion as flake input

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     crane.url = "github:ipetkov/crane/master";
+    corrosion.url = "github:corrosion-rs/corrosion?ref=v0.4.10";
+    corrosion.flake = false;
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
@@ -36,17 +38,10 @@
           formatter = pkgs.nixfmt-rfc-style;
           packages =
             let
-              corrosionSrc = builtins.fetchTarball {
-                url = "https://github.com/corrosion-rs/corrosion/archive/refs/tags/v0.4.tar.gz";
-                sha256 = "sha256:1avfjaxgqx05fv2jqbqnk20rkyhhzq0r7kp9xyimqdvmgajarh3p";
-              };
-
-              #corrosionSrc = /home/pschuster/dev/corrosion;
               rustLib = pkgs.callPackage ./nix/build-rust.nix { craneLib = inputs.crane.mkLib pkgs; };
               cxxbridge-cmd = pkgs.callPackage ./nix/build-cxxbridge-cmd.nix { inherit rustLib; };
-
               cmakeProj = pkgs.callPackage ./nix/build-cmake-proj.nix {
-                inherit corrosionSrc;
+                corrosionSrc = inputs.corrosion;
                 inherit rustLib;
                 inherit cxxbridge-cmd;
               };
